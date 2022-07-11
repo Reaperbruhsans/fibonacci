e4e9cfe6919dea2d1e61842baa76e76d799a3b68
Reaperbruhsans <107920979+Reaperbruhsans@users.noreply.github.com>
2022-07-11 16:16:40 +0300

    Create .C

---
+++ .C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibonacci (int length) {
+int* fibonacci = (int*)malloc(sizeof(int) * length);
+
+fibonacci[0] = 0;
+fibonacci[1] = 1;
+
+for (int i = 2; i < 10; i++) {
+fibonacci[i] = fibonacci[i - 1] + fibonacci[i- 2];
+}
+return 0;
+}
+
+int main() {
+int* fib fibonacci(10);
+
+for (int i = 0; i < 10; i++) {
+printf("%d\n", fib[i]);
+}
+return 0;
+}
