44ea56a8b486d861fd8b603c9edbc51edaaa43ef
Reaperbruhsans <107920979+Reaperbruhsans@users.noreply.github.com>
2022-07-11 16:18:23 +0300

    Update .C

--- .C
+++ .C
@@ -8,7 +8,7 @@ fibonacci[0] = 0;
 fibonacci[1] = 1;
 
 for (int i = 2; i < 10; i++) {
-fibonacci[i] = fibonacci[i - 1] + fibonacci[i- 2];
+   fibonacci[i] = fibonacci[i - 1] + fibonacci[i- 2];
 }
 return 0;
 }
@@ -17,7 +17,7 @@ int main() {
 int* fib fibonacci(10);
 
 for (int i = 0; i < 10; i++) {
-printf("%d\n", fib[i]);
+   printf("%d\n", fib[i]);
 }
 return 0;
 }
